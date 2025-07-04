b90c34a885e3533de01c71433a8107d700ce76b8
Jonathan Seibl <jonathan.seibl@edu.tbz.ch>
2025-07-04 09:49:41 +0200

    Created basic main

--- Escape_the_Mind/src/Main.java
+++ Escape_the_Mind/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        javax.swing.SwingUtilities.invokeLater(() -> new GUI());
+
+        Scanner scanner = new Scanner(System.in);
+        boolean playing = true;
+
+        System.out.println("Welcome to the Mind AZ");
+        System.out.println("You blablabla");
+
+        while (playing) {
+
+            //da müssted denn die aktuelle rühm ufgrüeft werde und so
+
+        }
+        scanner.close();
     }
 }
